6a1ed11156a0cd3aa8d12c24122afe39edfc42f8
huseynmatrix <huseynahmedov32@gmail.com>
2025-10-27 19:54:08 +0400

    add payment4 and payment 2 logic new

--- src/PaymentService.java
+++ src/PaymentService.java
@@ -2,4 +2,16 @@ public class PaymentService {
     public String pay() {
         return "Successfully for feature_1";
     }
+
+    public String pay2() {
+        return "Successfully for feature_2";
+    }
+
+    public String pay3() {
+        return "Successfully for feature_3";
+    }
+
+    public String pay4() {
+        return "Successfully for feature_4";
+    }
 }
